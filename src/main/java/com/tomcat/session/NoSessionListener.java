e67d0ad9cec86b66fe2a441dcc0fc89de5557c29
lsw0742 <lsw0742@yeah.net>
2017-04-27 15:00:07 +0800

    .

--- src/main/java/com/tomcat/session/NoSessionListener.java
+++ src/main/java/com/tomcat/session/NoSessionListener.java
@@ -5,13 +5,20 @@ import javax.servlet.http.HttpSessionBindingEvent;
 
 public class NoSessionListener implements HttpSessionAttributeListener{
 
+	private ThreadLocal<String> threadLocal = new ThreadLocal<String>();
 	@Override
 	public void attributeAdded(HttpSessionBindingEvent se) {
+		threadLocal.set(se.getName());
+		se.getSession().removeAttribute(se.getName());		
 		throw new UnsupportedOperationException("NoSessionManager Managed Session Unsupport [attributeAdded].");
 	}
 
 	@Override
 	public void attributeRemoved(HttpSessionBindingEvent se) {
+		if(se.getName().equals(threadLocal.get())){
+			threadLocal.remove();
+			return;
+		}
 		throw new UnsupportedOperationException("NoSessionManager Managed Session Unsupport [attributeRemoved].");
 	}
 
